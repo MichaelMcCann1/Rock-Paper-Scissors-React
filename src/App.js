6bcd0ef10c5529b42d66513e5262e4fd96fcaa4f
Michael McCann <81657333+MichaelMcCann1@users.noreply.github.com>
2021-10-03 12:12:14 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,107 +1,47 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 function App() {
   const [round, setRound] = useState(0)
   const [humanScore, setHumanScore] = useState(0)
   const [ties, setTies] = useState(0)
   const [computerScore, setComputerScore] = useState(0)
-  const [playerSelection, setPlayerSelection] = useState()
-  const [computerSelection, setComputerSelection] = useState()
-  const [humanImg, setHumanImg] = useState("Images/rock.png")
-  const [computerImg, setComputerImg] = useState("Images/rock.png")
-  const [humanImgClass, setHumanImgClass] = useState('humanRock')
-  const [compImgClass, setCompImgClass] = useState('compRock')
-  const [humanImgStyles, setHumanImgStyles] = useState()
-  const [computerImgStyles, setComputerImgStyles] = useState()
-  const [firstLoad, setFirstLoad] = useState(true)
-  const [gameIsActive, setGameIsActive] = useState(false)
+  const [playerSelection, setPlayerSelection] = useState(0)
+  const [computerSelection, setComputerSelection] = useState(0)
+  const [shaking, setShaking] = useState(false)
 
-  const updateImgs = function(){
-    switch(playerSelection){
-      case(0) : 
-        setHumanImg("Images/rock.png") 
-        setHumanImgClass('humanRock')
-        break
-      case(1) : 
-        setHumanImg("Images/paper.png")
-        setHumanImgClass('humanPaper')
-        break
-      case(2) : 
-        setHumanImg("Images/scissors.png")
-        setHumanImgClass('humanScissors')
-        break
-      default: 
-        setHumanImg("Images/rock.png") 
-        setHumanImgClass('humanRock')
-    }
-
-    switch(computerSelection){
-      case(0) : 
-        setComputerImg("Images/rock.png") 
-        setCompImgClass("compRock")
-        break
-      case(1) : 
-        setComputerImg("Images/paper.png")
-        setCompImgClass("compPaper")
-        break
-      case(2) : 
-        setComputerImg("Images/scissors.png")
-        setCompImgClass("compScissors")
-        break
-      default: 
-        setComputerImg("Images/rock.png") 
-        setCompImgClass("compRock")
-    }
-
-    setHumanImgStyles({})
-    setComputerImgStyles({})
-  }
-
-  const shake = function(){
-    setHumanImg("Images/rock.png") 
-    setHumanImgClass('humanRock')
-    setComputerImg("Images/rock.png") 
-    setCompImgClass("compRock")
-    setHumanImgStyles({animation: 'shakePlayer .5s 3'})
-    setComputerImgStyles({animation: 'shakeComp .5s 3'})
-  }
-
-  const calcWinner = function(){
-    let diff = Math.abs(playerSelection - computerSelection)
+  const calcWinner = function(player, comp){
+    let diff = Math.abs(player - comp)
     switch(diff){
       case(0): setTies(ties => ties + 1)
         break
-      case(1): (playerSelection > computerSelection) ? setHumanScore(score => score + 1) : setComputerScore(score => score + 1)
+      case(1): (player > comp) ? setHumanScore(score => score + 1) : setComputerScore(score => score + 1)
         break
-      default: (playerSelection < computerSelection) ? setHumanScore(score => score + 1) : setComputerScore(score => score + 1)
+      default: (player < comp) ? setHumanScore(score => score + 1) : setComputerScore(score => score + 1)
       }
   }
-  
-  const playGame = function(){
-    if (!firstLoad && playerSelection !== undefined && gameIsActive === false) {
-      setGameIsActive(true)
-      setRound(round => round + 1)
-      setComputerSelection(Math.floor(Math.random() * 3))
-      shake()
-        
-      setTimeout(() => {
-        updateImgs()
-        calcWinner()
-        setGameIsActive(false)
-        setPlayerSelection()
-      }, 1400)
-    }
+
+  const playGame = function(selection){
+    setShaking(true)
+    setPlayerSelection()
+    setComputerSelection()
+    setRound(round => round + 1)
+
+    setTimeout(() => {
+      setShaking(false)
+      setPlayerSelection(selection)
+      let randomNumber = Math.floor(Math.random() * 3)
+      setComputerSelection(randomNumber)
+      calcWinner(selection, randomNumber)
+    }, 1400)
   }
-
-  useEffect(() => {
-    playGame()
-    setFirstLoad(false)
-  }, [playerSelection])
-
 
   return (
     <div className="App">
+      <a className="githubLink" href="https://github.com/MichaelMcCann1/Rock-Paper-Scissors-React"><img src="Images/GitHub.svg"></img></a>
       <section className="round">
         <h1 className="roundNumber">{`Round ${round}`}</h1>
       </section>
@@ -120,13 +60,19 @@ function App() {
         </div>
       </section>
       <section className="hands">
-        <img src={humanImg} className={humanImgClass} style={humanImgStyles} alt="rock"></img>
-        <img src={computerImg} className={compImgClass} style={computerImgStyles} alt="rock"></img>
+        {shaking && <img src="/Images/rock.png" className="humanRock" style={{animation: 'shakePlayer .5s 3'}} alt="rock"></img>}
+        {shaking && <img src="/Images/rock.png" className="compRock" style={{animation: 'shakeComp .5s 3'}} alt="rock"></img>}
+        {playerSelection === 0 && <img src="/Images/rock.png" className="humanRock" alt="rock"></img>}
+        {playerSelection === 1 && <img src="/Images/paper.png" className="humanPaper" alt="rock"></img>}
+        {playerSelection === 2 && <img src="/Images/scissors.png" className="humanScissors" alt="rock"></img>}
+        {computerSelection === 0 && <img src="/Images/rock.png" className="compRock" alt="rock"></img>}
+        {computerSelection === 1 && <img src="/Images/paper.png" className="compPaper" alt="rock"></img>}
+        {computerSelection === 2 && <img src="/Images/scissors.png" className="compScissors" alt="rock"></img>}
       </section>
       <section className="buttons">
-        <button className="rock" onClick={() => setPlayerSelection(0)}>Rock</button>
-        <button className="paper" onClick={() => setPlayerSelection(1)}>Paper</button>
-        <button className="scissors" onClick={() => setPlayerSelection(2)}>Scissors</button>
+        <button className="rock" onClick={() => playGame(0)}>Rock</button>
+        <button className="paper" onClick={() => playGame(1)}>Paper</button>
+        <button className="scissors" onClick={() => playGame(2)}>Scissors</button>
       </section>
     </div>
   );
